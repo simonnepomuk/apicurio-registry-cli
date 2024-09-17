018b024ff38e24096f9d978165e780b62aa6e0c7
simon.wagner <simon.wagner@rise-world.com>
2024-09-17 02:07:50 +0200

    test: add download test

--- test/commands/artifact/download.test.ts
+++ test/commands/artifact/download.test.ts
@@ -1,14 +1,22 @@
 import {runCommand} from '@oclif/test'
 import {expect} from 'chai'
+import nock from "nock";
 
 describe('artifact:download', () => {
-  it('runs artifact:download cmd', async () => {
-    const {stdout} = await runCommand('artifact:download')
-    expect(stdout).to.contain('hello world')
-  })
+    const registryUrl = 'http://localhost:8080'
+    const apiPath = '/apis/registry/v2'
+    const registryApiUrl = `${registryUrl}${apiPath}`
 
-  it('runs artifact:download --name oclif', async () => {
-    const {stdout} = await runCommand('artifact:download --name oclif')
-    expect(stdout).to.contain('hello oclif')
-  })
+    it('downloads latest when not providing version', async () => {
+        const groupId = 'group-id'
+        const artifactName = 'artifact-name'
+        const artifactTag = `${groupId}/${artifactName}`
+        const expectedReturnValue = {id: 'FAKE_ARTIFACT'};
+        nock(registryApiUrl)
+            .get(`/groups/${groupId}/artifacts/${artifactName}`)
+            .reply(200, expectedReturnValue);
+
+        const {stdout} = await runCommand(`artifact:download ${artifactTag} --registry ${registryUrl}`)
+        expect(stdout).to.contain(JSON.stringify(expectedReturnValue, null, 2))
+    })
 })
